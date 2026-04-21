1558a096878f4e2e19a8b326312080e40e44bb3d
Ryan Ramdehol <ravinashwork@gmail.com>
2026-04-21 13:19:35 -0400

    projected initialized with directory structure

---
+++ app/main.py
@@ -0,0 +1,25 @@
+# ---------------------------- IMPORTS ------------------------------- #
+#
+# --------------.env file access flat string--------------#
+# Allows you to read the .env file
+# from dotenv import load_dotenv
+# import os
+# variable = os.getenv("<ENV VARIABLE>")
+# load_dotenv()
+# --------.env.json file access for JSON structure--------#
+# .env.json file
+# import json
+# with open('.env.json') as f:
+#     config = json.load(f)
+# value = config.get('YOUR_KEY')
+# --------Dynaconf--------#
+# Used to return a printed report for Dynaconf for debugging
+# from dynaconf import inspect_settings
+# inspect_settings(settings, print_report=True)
+# ---------------------------- CONSTANTS ------------------------------- #
+
+# ---------------------------- GLOBAL VARIABLES ------------------------------- #
+
+# ---------------------------- FUNCTIONS ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
